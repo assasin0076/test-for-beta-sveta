a1a29f2aba0d0490db755af6b235aefe95096acf
Victor <viktor.generalov@nlogic.ai>
2023-01-31 19:17:53 +0400

    feat: fix: добавлен тип для плейса

--- src/stores/placesStore.ts
+++ src/stores/placesStore.ts
@@ -2,10 +2,15 @@ import { ref } from "vue"
 import { defineStore } from "pinia"
 import axios from "axios"
 
+type TPlace = {
+  place_id: number
+  display_name: string
+}
+
 export const usePlacesStore = defineStore("places", () => {
-  const places = ref<object[]>([])
+  const places = ref<TPlace[]>([])
   const getPlaces = async (q: string) => {
-    const { data } = await axios.get<object[]>(`https://nominatim.openstreetmap.org/search?q=${q}&format=json`)
+    const { data } = await axios.get<TPlace[]>(`https://nominatim.openstreetmap.org/search?q=${q}&format=json`)
     places.value = data
   }
 
