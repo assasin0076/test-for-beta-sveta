5a6ec19c3c2f756ae6baaa8a82bf92ce448c7934
Victor <viktor.generalov@nlogic.ai>
2023-01-31 17:49:13 +0400

    chore: добавлен eslint/prettier

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
-import { RouterView } from "vue-router";
+import { RouterView } from "vue-router"
 </script>
 
 <template>
-  <RouterView />
+  <div class="page-container">
+    <RouterView />
+  </div>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
+.page-container {
+  min-height: 100vh;
+  min-width: 100vw;
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,20 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>a</div>
+  <div class="home-page">
+    <input type="text" />
+    <div class="home-content-container">
+      <div class="home-content-element"></div>
+      <div class="home-content-element"></div>
+      <div class="home-content-element"></div>
+    </div>
+  </div>
 </template>
+
+<style lang="scss">
+.home-page {
+  padding: 24px;
+  display: flex;
+  justify-content: center;
+}
+</style>
